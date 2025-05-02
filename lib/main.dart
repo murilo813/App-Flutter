6c11a24eaccb68638410c849376d44910c0aa22a
murilo813 <murilodesouza555@gmail.com>
2025-05-02 17:39:57 -0300

    Primeiro commit - enviando projeto Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,30 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
 import '../services/sync_service.dart';
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inicializa o serviço em segundo plano
-  await _initializeBackgroundService();
-
-  // Sincroniza os dados das lojas assim que o app iniciar
+  
   await syncEstoqueDeTodasLojas();
 
   runApp(MyApp());
 }
 
-Future<void> _initializeBackgroundService() async {
-  FlutterBackgroundService().configure(
-    androidConfiguration: AndroidConfiguration(
-      onStart: onServiceStart,
-      isInBackgroundMode: true,
-    ),
-    iosConfiguration: IosConfiguration(
-      onBackground: onServiceStart,
-      onForeground: onServiceStart,
-    ),
-  );
-
-  FlutterBackgroundService().start();
-}
-
-void onServiceStart() {
-  Timer.periodic(Duration(minutes: 15), (_) {
-    syncEstoqueDeTodasLojas();
-  });
-
-  print("Serviço em segundo plano iniciado...");
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AgroZecão',
+      home: HomePage(),
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+    );
+  }
 }
