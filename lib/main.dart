44d863db06374da5135e97a2184c65a3c3bf523c
murilo813 <murilodesouza555@gmail.com>
2025-05-02 17:18:14 -0300

    Primeiro commit - enviando projeto Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,41 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import '../services/sync_service.dart';
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
+  // Inicializa o serviço em segundo plano
+  await _initializeBackgroundService();
+
+  // Sincroniza os dados das lojas assim que o app iniciar
   await syncEstoqueDeTodasLojas();
 
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AgroZecão',
-      home: HomePage(),
-      supportedLocales: const [
-        Locale('pt', 'BR'),
-      ],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-    );
-  }
+Future<void> _initializeBackgroundService() async {
+  FlutterBackgroundService().configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onServiceStart,
+      isInBackgroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      onBackground: onServiceStart,
+      onForeground: onServiceStart,
+    ),
+  );
+
+  FlutterBackgroundService().start();
+}
+
+void onServiceStart() {
+  Timer.periodic(Duration(minutes: 15), (_) {
+    syncEstoqueDeTodasLojas();
+  });
+
+  print("Serviço em segundo plano iniciado...");
 }
